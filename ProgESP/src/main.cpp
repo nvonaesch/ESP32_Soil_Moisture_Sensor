0e4c43f12e0d3fdc45c9f96fa011c028105f4655
nvonaesch <nicolas.vonaesch1@gmail.com>
2024-11-25 21:11:12 +0100

    Ajout test BME280 fonctionne

--- ProgESP/src/main.cpp
+++ ProgESP/src/main.cpp
@@ -7,7 +7,9 @@
 #include <WiFi.h>
 #include <BluetoothSerial.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_BME280.h>
 #include <Preferences.h>
+#include <Wire.h>
 #include "TCPSocket.h"
 
 //#define TestBluetooth
@@ -16,7 +18,71 @@
 //#define TestGarderLogs
 //#define TestBouton
 //#define TestCapteurLuminosite
-#define ProgrPrincipal
+//#define ProgrPrincipal
+//#define TestBME280
+
+#ifdef TestBME280
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme;
+
+unsigned long delayTime;
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println(F("BME280 test"));
+
+    unsigned status;
+	
+    status = bme.begin(0x76);  
+
+    if (!status) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
+        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
+        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+        Serial.print("        ID of 0x60 represents a BME 280.\n");
+        Serial.print("        ID of 0x61 represents a BME 680.\n");
+        while (1) delay(10);
+    }
+    
+    Serial.println("-- Default Test --");
+    delayTime = 1000;
+
+    Serial.println();
+}
+
+void printValues() {
+    Serial.print("Temperature = ");
+    Serial.print(bme.readTemperature());
+    Serial.println(" °C");
+
+    Serial.print("Pressure = ");
+
+    Serial.print(bme.readPressure() / 100.0F);
+    Serial.println(" hPa");
+
+    Serial.print("Approx. Altitude = ");
+    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.print("Humidity = ");
+    Serial.print(bme.readHumidity());
+    Serial.println(" %");
+
+    Serial.println();
+}
+
+void loop() { 
+    printValues();
+    delay(delayTime);
+}
+
+
+#endif
+
+
 
 
 #ifdef TestBluetooth
